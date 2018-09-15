f910d9beb6bef866d2901787d5bde64f229b5671
Michael Davis <mcd164@miami.edu>
2018-09-14 22:54:17 -0400

    Tear out the gen stuff

--- lib/satchel.ex
+++ lib/satchel.ex
@@ -1,18 +1,2 @@
 defmodule Satchel do
-  @moduledoc """
-  Documentation for Satchel.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Satchel.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/satchel_test.exs
+++ test/satchel_test.exs
@@ -1,8 +1,4 @@
 defmodule SatchelTest do
   use ExUnit.Case
   doctest Satchel
-
-  test "greets the world" do
-    assert Satchel.hello() == :world
-  end
 end
